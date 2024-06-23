c765031881177e0919e0a0282138156a38e7a37a
Sujay Jeppu <sujayrj@gmail.com>
2024-06-23 23:02:12 +0530

    first commit

---
+++ catalog-service/src/test/java/dev/jeppu/bookstore/catalog/TestcontainersConfiguration.java
@@ -0,0 +1,13 @@
+package dev.jeppu.bookstore.catalog;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+
+class TestcontainersConfiguration {
+
+
+}
